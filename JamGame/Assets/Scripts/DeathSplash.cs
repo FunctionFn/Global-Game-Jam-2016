a5f2c8c9f5b1b27d2d99c97e3a53918ed5a0ac0b
AJ Klopfenstein <aj.klopfenstein@gmail.com>
2016-01-31 07:41:50 -0600

    Linked death screen to main title

---
+++ JamGame/Assets/Scripts/DeathSplash.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class DeathSplash : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.anyKeyDown)
+		{
+			SceneManager.LoadScene("TitleScreen");
+		}
+	}
+}
